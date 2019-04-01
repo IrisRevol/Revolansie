9bcdf51c58ea69668622cf0d092f4312bdd313b8
Claire Dansie <claire.dansie@hotmail.co.uk>
2019-04-01 18:56:02 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
 $(function () {
-
-    $("#button") .on("click", function(){
+    $("#button-contact").on("click", function(){
       alert("Thanks for getting in touch with us - we will try and get back to you as soon as we can!")
     });
 });
 $(function () {
-
-    $("#button") .on("click", function(){
-      alert1("Thanks for logging your emotions - and happy offloading!")
+    $("#button").on("click", function(){
+      alert("Thanks for logging your emotions and happy offloading!")
     });
 });
