52f46bbf8e81abd42b21e7a8937ede9f39e43860
Claire Dansie <claire.dansie@hotmail.co.uk>
2019-04-01 18:14:56 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,9 @@ $(function () {
       alert("Thanks for getting in touch with us - we will try and get back to you as soon as we can!")
     });
 });
+$(function () {
+
+    $("#button") .on("click", function(){
+      alert1("Thanks for logging your emotions - and happy offloading!")
+    });
+});
