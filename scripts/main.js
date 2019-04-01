f86561a151ffc3fad15f87d11272e129f1525c4e
IrisRevol <47482538+IrisRevol@users.noreply.github.com>
2019-04-01 13:45:25 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
-$("#button").on("click", function(){
-  alert("Thanks for getting in touch with us - we will try and get back to you as soon as we can!")
+$(function () {
+
+    $("#button") .on("click", function(){
+      alert("Thanks for getting in touch with us - we will try and get back to you as soon as we can!")
+    });
 });
